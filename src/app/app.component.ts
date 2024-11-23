5e1e76368c40efbd53e4498b2a512e7a87f902eb
Clenio <cleniofilho87@gmail.com>
2024-11-23 11:38:19 -0300

    Criação do trailer e ajustes nas posições do inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from '../componentes/menu/menu.component';
 import { AMelhorComponent } from "../componentes/amelhor/amelhor.component";
 import { AutoraisComponent } from "../componentes/autorais/autorais.component";
+import { TrailerComponent } from '../componentes/trailer/trailer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, AMelhorComponent, AutoraisComponent],
+  imports: [RouterOutlet, MenuComponent, AMelhorComponent, AutoraisComponent, TrailerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
