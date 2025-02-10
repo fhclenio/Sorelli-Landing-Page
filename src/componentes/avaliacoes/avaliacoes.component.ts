fa63de8ac70dc62416bc4c9342a1ebf24a3af6e2
fhclenio <cleniofilho87@gmail.com>
2025-02-09 21:35:57 -0300

    Ajustes finos

--- src/componentes/avaliacoes/avaliacoes.component.ts
+++ src/componentes/avaliacoes/avaliacoes.component.ts
@@ -11,9 +11,9 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 export class AvaliacoesComponent {
 
   reviews = [
-    { id: 1, name: 'João', comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet aliquet nulla at condimentum. Praesent nec nulla vulputate, fermentum erat ac, commodo justo. Nam quis tortor varius, pulvinar ante vel, scelerisque ipsum. Sed sit amet pretium sapien. Maecenas sed laoreet metus, et tempor nulla. Sed porta sapien felis, vitae consectetur.  Sed porta sapien felis, vitae consectetur.' },
-    { id: 2, name: 'Maria', comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nibh libero, imperdiet id semper eleifend, dapibus in ipsum. Donec accumsan libero vel enim placerat cursus. Vestibulum ut congue turpis. Pellentesque lacinia, est nec hendrerit semper, dolor lorem scelerisque orci, at vulputate nisi metus a mi. Etiam eros tortor, consectetur sit.' },
-    { id: 3, name: 'Carlos', comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper ex in justo placerat imperdiet. Mauris ut lacus vitae massa dignissim iaculis. Nulla eget malesuada lorem. Maecenas ut gravida odio. Sed a quam eget turpis feugiat sagittis eget sed diam. Aenean in elit sapien. Fusce dictum ut ipsum sit amet rhoncus. In hac habitasse platea dictumst. Aliquam at est sed sapien scelerisque eleifend. Proin massa turpis, tincidunt nec risus eu.' },
+    { id: 1, name: 'João', comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet.' },
+    { id: 2, name: 'Maria', comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    { id: 3, name: 'Carlos', comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
     { id: 4, name: 'Pedro', comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }
   ];
 
--- src/componentes/metricas/metricas.component.ts
+++ src/componentes/metricas/metricas.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './metricas.component.css'
 })
 export class MetricasComponent {
+    
+    anos = 0;
 
+    ngAfterViewInit() {
+        const dataFundacao = new Date(2017, 10, 12);
+        const dataAtual = new Date();
+
+        let diferencaAnos = dataAtual.getFullYear() - dataFundacao.getFullYear();
+
+        const aniversarioAindaNaoChegou =
+          dataAtual.getMonth() < dataFundacao.getMonth() ||
+          (dataAtual.getMonth() === dataFundacao.getMonth() && dataAtual.getDate() < dataFundacao.getDate());
+
+        if (aniversarioAindaNaoChegou) 
+          diferencaAnos--;
+
+        this.anos = diferencaAnos;
+    }
 }
